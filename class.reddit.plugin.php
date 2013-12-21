27d245910ab5732837e5b2a8c74267fe75841639
Kasper Isager <kasperisager@gmail.com>
2013-12-21 16:33:43 -0500

    Bump version in preparation for eventual release

--- class.reddit.plugin.php
+++ class.reddit.plugin.php
@@ -4,7 +4,7 @@
 $PluginInfo['Reddit'] = array(
     'Name'                 => 'Reddit Social Connect',
     'Description'          => 'Users may sign into your site using their Reddit account.',
-    'Version'              => '0.0.3',
+    'Version'              => '0.1.0',
     'Author'               => "Adrian Speyer",
     'RequiredApplications' => array('Vanilla' => '2.1b2'),
     'RequiredTheme'        => false,
